fab6cc1d4ac04c6ea85e5f6393a0022fdbc4056d
Gurpreet Singh <gurpreets0610@gmail.com>
2020-04-25 22:28:22 +0530

    Update Final

---
+++ controller/Utils.py
@@ -0,0 +1,67 @@
+import os
+import subprocess
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+
+def get_pull():
+    p_return=0
+    with open("controller/logs.txt", 'wb') as logfile:
+        p_return=subprocess.call(['git','pull'] ,stdout=logfile, stderr=subprocess.STDOUT)
+    if(p_return!=0):
+        email_it("Git Pull")
+        return 
+    return buildUp()
+    
+    
+def buildUp():
+    with open("controller/logs.txt", 'wb') as logfile:
+        p_return=subprocess.call(['docker-compose','build'] ,stdout=logfile, stderr=subprocess.STDOUT)
+    if(p_return!=0):
+        email_it("Build")
+        return RollBack()
+     
+    with open("controller/logs.txt", 'wb') as logfile:
+        p_return=subprocess.call(['docker-compose','up','-d'] ,stdout=logfile, stderr=subprocess.STDOUT)
+    if(p_return!=0):
+        email_it("Runtime")
+        return RollBack()
+    print("Done ALL")
+    return 
+    
+def RollBack():
+    p_return=0
+    with open("controller/logs.txt", 'wb') as logfile:
+        p_return=subprocess.call(['git','reset','--hard','HEAD@{1}'] ,stdout=logfile, stderr=subprocess.STDOUT)
+    if(p_return!=0):
+        email_it("RollBack")
+        return
+    buildUp()
+   
+
+def email_it(problem):
+    print(problem+ " Error")
+    message = MIMEMultipart()
+    message['Subject'] = 'Automatic deployment failed.'
+    message['From'] = 'imailgurpreet@gmail.com'
+    message['To'] = 'gurpreets8117@else.com'
+    f=open("controller/logs.txt", "r")
+    fail="""<h2> Flask Web Application - <b> {}</b> Failed.<br/></h2>
+            <br/>
+            Check console <a href="">output</a> to view full results.<br/>
+            If you cannot connect to the build server, check the attached logs.<br/>
+            <br/>
+            --<br/>
+            Following is the last lines of the log.<br/>
+            <br/>
+            --LOG-BEGIN--<br/>
+            <pre style='line-height: 22px; display: block; color: #333; font-family: Monaco,Menlo,Consolas,"Courier New",monospace; padding: 10.5px; margin: 0 0 11px; font-size: 13px; word-break: break-all; word-wrap: break-word; white-space: pre-wrap; background-color: #f5f5f5; border: 1px solid #ccc; border: 1px solid rgba(0,0,0,.15); -webkit-border-radius: 4px; -moz-border-radius: 4px; border-radius: 4px;'>{}</pre>
+            --LOG-END-- """.format(problem,f.read())
+    message.attach(MIMEText(fail, 'html') )       
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login("mrsingh8117@gmail.com", "Qwerty123@")             
+    # server.sendmail(“sender@gmail.com”, “receiver@gmail.com”, “YourMessage”)
+    server.sendmail("mrsingh8117@gmail.com", "gurpreets8117@gmail.com", message.as_string())
--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-from flask import Flask,  jsonify
-from flask import render_template # for render_template
-from flask import request # for getting user input from HTML page
-import requests
+from flask import Flask, request
 import json
-import os
-
-"""python2 pagekite.py  3000 gurpreetsingh.pagekite.me"""
+from controller.Utils import *
 app = Flask(__name__)
 
 @app.route('/')
@@ -21,17 +16,7 @@ def payload():
     data = request.data
     if(json.loads(data)["push"] == "True"):
         print("hey Lets PULL")
-        x=os.system("git pull")
-        print(x)
-        y=os.system("cd /home/gurpreet/Code/github/DockerWebApp/ && docker-compose up -d ")
-        print("Error Code ->"+str(y))
-        if(y!=0):
-            print("Error Occured During Build")
-            z=os.system("cd /home/gurpreet/Code/github/DockerWebApp/ && git reset --hard HEAD@{1} ")
-            y=os.system("cd /home/gurpreet/Code/github/DockerWebApp/ && docker-compose up -d ")
-            print("Error Code ->"+str(y))
-            
-            
+        get_pull()
        
     return json.dumps({"success":True})
 
